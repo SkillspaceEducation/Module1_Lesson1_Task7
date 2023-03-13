27bb5479db5e8165b388fc2e811676505c783535
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-13 11:59:31 +0300

    Написать метод, принимающий на вход координаты двух точек и возвращающий длину отрезка между этими точками. При тестировании координаты вводить с клавиатуры. Подсказка: использовать класс Math и формулу расчета отрезка по координатам двух точек.
    Выполнен рефакторинг кода.

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координаты первой точки (два целых числа, через пробел)");
-        int point1_a = scanner.nextInt();
-        int point2_a = scanner.nextInt();
+        int pointA1 = scanner.nextInt();
+        int pointA2 = scanner.nextInt();
         System.out.println("Введите координаты второй точки (два целых числа, через пробел)");
-        int point1_b = scanner.nextInt();
-        int point2_b = scanner.nextInt();
+        int pointB1 = scanner.nextInt();
+        int pointB2 = scanner.nextInt();
         scanner.close();
-        double result = ptpDistance(point1_a,point2_a,point1_b,point2_b);
-        System.out.printf("Расстояние между точками с координатами (%d,%d) и (%d,%d) равно %f",point1_a,point2_a,point1_b,point2_b,result);
+        double result = ptpDistance(pointA1,pointA2,pointB1,pointB2);
+        System.out.printf("Расстояние между точками с координатами (%d,%d) и (%d,%d) равно %f",
+                pointA1,pointA2,pointB1,pointB2,result);
     }
-    static double ptpDistance(int point1_a,int point2_a,int point1_b,int point2_b){
+    static double ptpDistance(int pointA1,int pointA2,int pointB1,int pointB2){
         int powValue = 2;
-        return Math.sqrt((Math.pow((point1_b - point1_a),powValue) + Math.pow((point2_b - point2_a),powValue))); // вычисление расстояния между двумя точками
+        return Math.sqrt((Math.pow((pointB1 - pointA1),powValue) +
+                Math.pow((pointB2 - pointA2),powValue))); // вычисление расстояния между двумя точками
     }
 }
